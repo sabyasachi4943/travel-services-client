3fccc9a46a3a52bd9ce627167ae2f619c0ca9f49
Sabyasachi Sarkar <54597679+sabyasachi4943@users.noreply.github.com>
2022-11-08 15:46:22 +0600

    first commit

---
+++ src/Layout/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <div>
+      <h3>main layout</h3>
+    </div>
+  );
+};
+
+export default Main;
---
+++ src/Pages/Home/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h3>about page</h3>
+    </div>
+  );
+};
+
+export default About;
