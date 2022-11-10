8264f4f273bd35336c05f66ca87c67370771f6ee
Sabyasachi Sarkar <54597679+sabyasachi4943@users.noreply.github.com>
2022-11-10 13:12:41 +0600

    home section 3 services

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import Services from '../../Services/Services';
-
+import React from "react";
+import Services from "../../Services/Services";
+import ShortService from "../../ShortService/ShortService";
 
 const Home = () => {
   return (
     <div>
-
-      <Services></Services>
+      <ShortService></ShortService>
+      {/* <Services></Services> */}
     </div>
   );
 };
---
+++ src/Pages/ShortService/ShortService.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Service from "../Service/Service";
+
+const ShortService = () => {
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/services")
+      .then((res) => res.json())
+      .then((data) => {
+        let newData = data.slice(0, 3);
+        setServices(newData);
+      });
+  }, []);
+
+  return (
+    <div className="">
+      <div className="d-flex justify-content-center flex-wrap p-3">
+        {services.map((service) => (
+          <Service
+            key={service._id}
+            service={service}
+            // handleAddService={handleAddService}
+          ></Service>
+        ))}
+      </div>
+      <div className="align-items-center justify-content-center">
+        <Button variant="warning">
+          <Link className="text-decoration-none" to="/services">
+            see all services
+          </Link>
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default ShortService;
